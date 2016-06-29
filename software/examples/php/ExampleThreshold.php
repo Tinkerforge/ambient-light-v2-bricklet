5288f7ade6669972f1f4c2aa28a62dd7c97b48a7
Matthias Bolte <matthias@tinkerforge.com>
2016-06-29 15:22:19 +0200

    Whitespace

--- software/examples/php/ExampleThreshold.php
+++ software/examples/php/ExampleThreshold.php
@@ -27,7 +27,8 @@ $ipcon->connect(HOST, PORT); // Connect to brickd
 $al->setDebouncePeriod(10000);
 
 // Register illuminance reached callback to function cb_illuminanceReached
-$al->registerCallback(BrickletAmbientLightV2::CALLBACK_ILLUMINANCE_REACHED, 'cb_illuminanceReached');
+$al->registerCallback(BrickletAmbientLightV2::CALLBACK_ILLUMINANCE_REACHED,
+                      'cb_illuminanceReached');
 
 // Configure threshold for illuminance "greater than 500 Lux" (unit is Lux/100)
 $al->setIlluminanceCallbackThreshold('>', 500*100, 0);
