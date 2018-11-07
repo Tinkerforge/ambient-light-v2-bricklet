88b0d6a710be144224eebecc4b755a33f2ee4fa9
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:18:29 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,33 +1,34 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ambient_light_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{ambient_light_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Ambient Light Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Ambient Light Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let ambient_light_v2_bricklet = AmbientLightV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let al = AmbientLightV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get threshold listeners with a debounce time of 10 seconds (10000ms)
-    ambient_light_v2_bricklet.set_debounce_period(10000);
+    // Get threshold receivers with a debounce time of 10 seconds (10000ms).
+    al.set_debounce_period(10000);
 
-    //Create listener for illuminance reached events.
-    let illuminance_reached_listener = ambient_light_v2_bricklet.get_illuminance_reached_receiver();
-    // Spawn thread to handle received events. This thread ends when the ambient_light_v2_bricklet
+    // Create receiver for illuminance reached events.
+    let illuminance_reached_receiver = al.get_illuminance_reached_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `al` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in illuminance_reached_listener {
-            println!("Illuminance: {}{}", event as f32 / 100.0, " lx");
+        for illuminance_reached in illuminance_reached_receiver {
+            println!("Illuminance: {} lx", illuminance_reached as f32 / 100.0);
             println!("Too bright, close the curtains!");
         }
     });
 
-    // Configure threshold for illuminance "greater than 500 lx"
-    ambient_light_v2_bricklet.set_illuminance_callback_threshold('>', 500 * 100, 0);
+    // Configure threshold for illuminance "greater than 500 lx".
+    al.set_illuminance_callback_threshold('>', 500 * 100, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
